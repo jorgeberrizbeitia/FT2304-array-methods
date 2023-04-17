2ecb755c6336b194bd7fc36e98bab9f929befb5b
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-04-17 12:06:26 +0200

    map

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log("probando")
 // .forEach()
 
 
-let randomNumbers = [ 4, 8, 15, 16, 23, 42 ];
+// let randomNumbers = [ 4, 8, 15, 16, 23, 42 ];
 
 
 // let testFunction = () => {
@@ -30,6 +30,7 @@ let randomNumbers = [ 4, 8, 15, 16, 23, 42 ];
 //   console.log("test de forEach")
 // }
 
+let randomNumbers = [ 4, 8, 15, 16, 23, 42 ];
 
 randomNumbers.forEach( (eachElement, index) => {
   console.log("test de forEach", eachElement, index)
@@ -39,4 +40,67 @@ randomNumbers.forEach( (eachElement, index) => {
   }
 
 } )
+
+
+// .map()
+
+let algo = randomNumbers.map( (eachElement) => {
+  console.log("ejecutando algo por cada elemento del array", eachElement)
+
+  let newValue = eachElement / 10
+  // con el map, nosotros debemos retornar algo.
+  // con ese algo, estaremos construyendo un nuevo array.
+  return newValue; // el valor modificado con el que yo quiero construir el nuevo array
+} )
+
+// estoy reconstruyendo el array
+// es un NUEVO array
+// el map nos permite hacer modificaciones en los elementos
+console.log(algo)
+console.log(randomNumbers)
+
+// function map(callback) {
+
+//   let newArr = []
+
+//   // tiene el array sobre el cual la ejecutamos
+//   for (let i = 0; i < array.length; i++) {
+//     callback()
+//     newArr.push(array[i])
+//   }
+
+//   return newArr
+// }
+
+
+// actividad guiada
+// crea una funcion que reciba un array de numeros, y retorne un nuevo array con los numeros convertidos en strings.
+
+function numbersAsString( numbersArr ) {
+
+  let mappedArray = numbersArr.map((eachNumber) => {
+    let numAsString = eachNumber.toString()
+    return numAsString
+  })  
+
+  return mappedArray
+
+}
+
+
+// test de la funcion
+let stringsArr = numbersAsString( [2, 10, 54] )
+console.log(stringsArr)
+
+
+let mappedArray = [2, 10, 54].map((eachNumber) => {
+  let numAsString = eachNumber.toString()
+  return numAsString
+})  
+
+console.log(mappedArray)
+
+
+
+
 
